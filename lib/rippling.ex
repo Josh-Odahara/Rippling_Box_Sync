6619fa06b3ce25e09c7f7e5e469d7ecff9aaefa0
Josh Odahara <josh.odahara@gmail.com>
2026-06-22 00:14:36 -0700

    add logic for Rippling.ex

---
+++ lib/rippling.ex
@@ -0,0 +1,31 @@
+defmodule RipplingBoxSync.Rippling do
+
+  def fetch_employee_docs(%Employee{associate_id: associate_id, first_name: first_name, last_name: last_name} = _employee) do
+  [%{
+    doc_type: "I-9",
+    extension: "pdf",
+    filename: build_filename(first_name, last_name, associate_id, "I-9", "pdf"),
+    content: "mock binary data"},
+  %{
+    doc_type: "W-2",
+    extension: "pdf",
+    filename: build_filename(first_name, last_name, associate_id, "W-2", "pdf"),
+    content: "mock binary data"},
+  %{
+    doc_type: "offer_letter",
+    extension: "pdf",
+    filename: build_filename(first_name, last_name, associate_id, "offer_letter", "pdf"),
+    content: "mock binary data"},
+  %{
+    doc_type: "i9_support",
+    extension: "jpg",
+    filename: "IMG_5545.jpg",
+    content: "mock binary data"}
+  ]
+  end
+
+  defp build_filename(first_name, last_name, associate_id, doc_type, extension) do
+    "Employee File - #{first_name} #{last_name} - #{associate_id} - #{doc_type}.#{extension}"
+  end
+
+end
