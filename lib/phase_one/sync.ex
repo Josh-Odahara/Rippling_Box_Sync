a2e289f04aba1418ccf143cb022c359efce038b4
Josh Odahara <josh.odahara@gmail.com>
2026-06-25 16:55:19 -0700

    Add sync.sync_employee, handles fetch/folder/upload with retry on failure

---
+++ lib/phase_one/sync.ex
@@ -0,0 +1,20 @@
+defmodule RipplingBoxSync.PhaseOne.Sync do
+  alias RipplingBoxSync.Rippling
+  alias RipplingBoxSync.Box
+
+  def sync_employee(%Employee{
+        first_name: _first_name,
+        last_name: _last_name,
+        associate_id: _associate_id}
+        = employee)
+        do
+        case Rippling.fetch_employee_docs(employee) do
+          {:ok, docs} ->
+            folder_path = Box.create_employee_folder(employee)
+            Enum.each(docs, fn doc -> Box.upload_file(folder_path, doc) end)
+          {:error, reason} ->
+            Box.create_retry_folder(employee, reason)
+        end
+      end
+
+end
