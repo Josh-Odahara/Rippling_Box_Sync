b9b41a6f6380adf02054d44fb14c64c7993f4987
Josh Odahara <josh.odahara@gmail.com>
2026-06-25 20:15:34 -0700

    Add Scanner.i9_related?, threshold check (score >= 3) on top of score_doc

--- lib/phase_two/scanner.ex
+++ lib/phase_two/scanner.ex
@@ -12,9 +12,20 @@ defmodule RipplingBoxSync.PhaseTwo.Scanner do
     keywords = Keywords.i9_keywords()
     downcased_keywords = Enum.map(keywords, fn keyword -> String.downcase(keyword) end)
 
-    filename_count = Enum.count(downcased_keywords, fn keyword -> String.contains?(downcased_filename, keyword) end)
-    content_count = Enum.count(downcased_keywords, fn keyword -> String.contains?(downcased_content, keyword) end)
+    filename_count =
+      Enum.count(downcased_keywords, fn keyword ->
+        String.contains?(downcased_filename, keyword)
+      end)
+
+    content_count =
+      Enum.count(downcased_keywords, fn keyword ->
+        String.contains?(downcased_content, keyword)
+      end)
 
     filename_count + content_count
   end
+
+ def i9_related?(doc) do
+   score_doc(doc) >= 3
+ end
 end
