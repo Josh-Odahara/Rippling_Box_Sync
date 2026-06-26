a48b4944f9dce512cd4301ee3b19e77f4e4a3d90
Josh Odahara <josh.odahara@gmail.com>
2026-06-25 19:02:10 -0700

    small fixes

--- lib/phase_one/sync.ex
+++ lib/phase_one/sync.ex
@@ -3,24 +3,26 @@ defmodule RipplingBoxSync.PhaseOne.Sync do
   alias RipplingBoxSync.Box
   alias RipplingBoxSync.CSVParser
 
-  def sync_employee(%Employee{
-        first_name: _first_name,
-        last_name: _last_name,
-        associate_id: _associate_id}
-        = employee)
-        do
-        case Rippling.fetch_employee_docs(employee) do
-          {:ok, docs} ->
-            folder_path = Box.create_employee_folder(employee)
-            Enum.each(docs, fn doc -> Box.upload_file(folder_path, doc) end)
-          {:error, reason} ->
-            Box.create_retry_folder(employee, reason)
-        end
-      end
+  def sync_employee(
+        %Employee{
+          first_name: _first_name,
+          last_name: _last_name,
+          associate_id: _associate_id
+        } =
+          employee
+      ) do
+    case Rippling.fetch_employee_docs(employee) do
+      {:ok, docs} ->
+        folder_path = Box.create_employee_folder(employee)
+        Enum.each(docs, fn doc -> Box.upload_file(folder_path, doc) end)
+
+      {:error, reason} ->
+        Box.create_retry_folder(employee, reason)
+    end
+  end
 
   def run(csv_path) do
     employees = CSVParser.parse(csv_path)
     Enum.each(employees, fn employee -> sync_employee(employee) end)
   end
-
 end
