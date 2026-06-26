d0d47533f02e30d6fcec5a560ea79a15120f828d
Josh Odahara <josh.odahara@gmail.com>
2026-06-25 19:00:20 -0700

    Add content search based off of keywords to Scanner.score_doc

--- lib/phase_two/scanner.ex
+++ lib/phase_two/scanner.ex
@@ -1,12 +1,20 @@
 defmodule RipplingBoxSync.PhaseTwo.Scanner do
   alias RipplingBoxSync.PhaseTwo.Keywords
 
-  def score_doc(%{doc_type: _doc_type, extension: _extension, filename: filename, content: _content}) do
+  def score_doc(%{
+        doc_type: _doc_type,
+        extension: _extension,
+        filename: filename,
+        content: content
+      }) do
     downcased_filename = String.downcase(filename)
+    downcased_content = String.downcase(content)
     keywords = Keywords.i9_keywords()
     downcased_keywords = Enum.map(keywords, fn keyword -> String.downcase(keyword) end)
-    Enum.count(downcased_keywords, fn keyword -> String.contains?(downcased_filename, keyword) end)
+
+    filename_count = Enum.count(downcased_keywords, fn keyword -> String.contains?(downcased_filename, keyword) end)
+    content_count = Enum.count(downcased_keywords, fn keyword -> String.contains?(downcased_content, keyword) end)
+
+    filename_count + content_count
   end
-
-
 end
