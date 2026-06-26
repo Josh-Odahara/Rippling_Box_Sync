7f1014ce93abefa0ad79306c7c76325047a775ee
Josh Odahara <josh.odahara@gmail.com>
2026-06-25 17:32:08 -0700

    Add Keywords.i9_keywords, configurable list of I-9 filname and contents matching

---
+++ lib/phase_two/keywords.ex
@@ -0,0 +1,15 @@
+defmodule RipplingBoxSync.PhaseTwo.Keywords do
+
+  @i9_keywords [
+    "I-9",
+    "employement eligibility",
+    "verification",
+    "passport",
+    "driver's license",
+    "social security",
+    "citizenship",
+    "identification card"
+  ]
+
+  def i9_keywords, do: @i9_keywords
+end
